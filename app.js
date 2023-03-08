339e30cbb33523c764d569c4d8dd4a78f031366d
DilnaDilin <dilna.dilin7@gmail.com>
2023-03-08 16:45:18 +0400

    'ayout and partials-set

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
+var userRouter = require('./routes/user');
 
 var app = express();
 const db=require('./config/connection');
+const {engine:hbs}=require('express-handlebars');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs', hbs ({extname: 'hbs',defaultLayout: 'layout', layoutsDir:__dirname + '/views/layout',partialsDir:__dirname+'/views/partials'}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,8 +30,8 @@ db.connect((err)=>{
   }
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
